4fcf583cefcfd81da5fd8d0c9420e1ea45ccdd19
Dominik <dominik24aa@gmail.com>
2019-11-08 18:16:49 +0100

    create first view

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <h1>toDoList.<span>pl</span></h1>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+h1 {
+  font-size: 60px;
+  text-align: center;
+  font-family: 'Roboto', sans-serif;
+    }
+span {
+    color: #28a745;
+}
+</style>
